8a1f3b7f751adb1865770f063f4ed0012625649c
Rita-B-O <ritabochaberi@gmail.com>
2024-07-07 11:26:02 +0300

    Program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,31 +8,31 @@
  */
 int main(void)
 {
-	int letter_1, letter_2, letter_3;
+int letter_1, letter_2, letter_3;
 
-	letter_1 = '0';
-	while (letter_1 < '8')
-	{
-		letter_2 = letter_1 + 1;
-		while (letter_2 < '9')
-		{
-			letter_3 = letter_2 + 1;
-			while (letter_3 <= '9')
-			{
-				putchar(letter_1);
-				putchar(letter_2);
-				putchar(letter_3);
-				if (letter_1 != '7' || letter_2 != '8' || letter_3 != '9')
-				{
-					putchar(',');
-					putchar(' ');
-				}
-				letter_3++;
-			}
-			letter_2++;
-		}
-		letter_1++;
-	}
-	putchar('\n');
-	return (0);
+letter_1 = '0';
+while (letter_1 < '8')
+{
+letter_2 = letter_1 + 1;
+while (letter_2 < '9')
+{
+letter_3 = letter_2 + 1;
+while (letter_3 <= '9')
+{
+putchar(letter_1);
+putchar(letter_2);
+putchar(letter_3);
+if (letter_1 != '7' || letter_2 != '8' || letter_3 != '9')
+{
+putchar(',');
+putchar(' ');
+}
+letter_3++;
+}
+letter_2++;
+}
+letter_1++;
+}
+putchar('\n');
+return (0);
 }
